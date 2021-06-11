kerneltoast/android_kernel_xiaomi_dipper

power: reset: Disable download mode
We never want to enter download mode on this device.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

drivers/power/reset/msm-poweroff.c
@@ -63,7 +63,7 @@ static void scm_disable_sdi(void);
  * There is no API from TZ to re-enable the registers.
  * So the SDI cannot be re-enabled when it already by-passed.
  */
-static int download_mode = 1;
+static int download_mode = !IS_ENABLED(CONFIG_BOARD_DIPPER);
 #else
 static const int download_mode;
 #endif

stormbreaker-project/kernel_asus_X00P-4.9

drivers/power/reset/msm-poweroff.c
@@ -63,7 +63,7 @@ static void scm_disable_sdi(void);
  * There is no API from TZ to re-enable the registers.
  * So the SDI cannot be re-enabled when it already by-passed.
  */
-static int download_mode = 1;
+static int download_mode = !IS_ENABLED(CONFIG_ARCH_MSM8937);
 #else
 static const int download_mode;
 #endif